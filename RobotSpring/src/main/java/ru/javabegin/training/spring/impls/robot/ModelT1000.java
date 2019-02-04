391ec71c2dcc9301fbdcde86e75692eacf20c50c
Andrey Morozov <32931484+AndreyMr@users.noreply.github.com>
2019-02-04 11:12:44 +0300

    add constructors and params

--- RobotSpring/src/main/java/ru/javabegin/training/spring/impls/robot/ModelT1000.java
+++ RobotSpring/src/main/java/ru/javabegin/training/spring/impls/robot/ModelT1000.java
@@ -11,6 +11,10 @@ public class ModelT1000 implements Robot {
 	private Leg leg;
 	private Head head;
 
+	private String color;
+	private int year;
+	private boolean soundEnable;
+
 	public ModelT1000() {
 	}
 
@@ -21,11 +25,79 @@ public class ModelT1000 implements Robot {
 		this.head = head;
 	}
 
+	public ModelT1000(Hand hand, Leg leg, Head head, String color, int year, boolean soundEnable) {
+		super();
+		this.hand = hand;
+		this.leg = leg;
+		this.head = head;
+		this.color = color;
+		this.year = year;
+		this.soundEnable = soundEnable;
+	}
+
+	public ModelT1000(String color, int year, boolean soundEnable) {
+		super();
+		this.color = color;
+		this.year = year;
+		this.soundEnable = soundEnable;
+	}
+
+	public Hand getHand() {
+		return hand;
+	}
+
+	public void setHand(Hand hand) {
+		this.hand = hand;
+	}
+
+	public Leg getLeg() {
+		return leg;
+	}
+
+	public void setLeg(Leg leg) {
+		this.leg = leg;
+	}
+
+	public Head getHead() {
+		return head;
+	}
+
+	public void setHead(Head head) {
+		this.head = head;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public boolean isSoundEnable() {
+		return soundEnable;
+	}
+
+	public void setSoundEnable(boolean soundEnable) {
+		this.soundEnable = soundEnable;
+	}
+
 	@Override
-	public void fire() {
+	public void action() {
 		head.calc();
 		hand.catchSomething();
 		leg.go();
+		System.out.println("Color: " + color);
+		System.out.println("Year: " + year);
+		System.out.println("Sound: " + soundEnable);
 	}
 
 	@Override
