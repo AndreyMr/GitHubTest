c59dabc6a4aa677187496fdeddfab66d1deb0bd9
Andrey Morozov <32931484+AndreyMr@users.noreply.github.com>
2019-02-11 12:32:03 +0300

    GIT home

--- RobotSpring/src/main/java/ru/javabegin/training/spring/main/Start.java
+++ RobotSpring/src/main/java/ru/javabegin/training/spring/main/Start.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import ru.javabegin.training.spring.impls.pool.T1000Pool;
+import ru.javabegin.training.spring.impls.robot.ModelT1000;
 
 /**
  * @author Андрей
@@ -12,9 +13,9 @@ import ru.javabegin.training.spring.impls.pool.T1000Pool;
 public class Start {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("all-context.xml");
-		// ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
-		// t1000.dance();
-		// t1000.action();
+		ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
+		t1000.dance();
+		t1000.action();
 		// System.out.println(t1000.getHead());
 
 		// ModelT1000 t1001 = (ModelT1000) context.getBean("t1000");
