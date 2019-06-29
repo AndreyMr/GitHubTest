c7aec669f5db0e9ec53c2f1a3f3c2f69ed23b442
Andrey Morozov <32931484+AndreyMr@users.noreply.github.com>
2019-06-29 18:15:49 +0300

    added new JSF project OnlineLibrary_JSF

---
+++ OnlineLibrary_JSF/src/main/webapp/bean/User.java
@@ -0,0 +1,24 @@
+package main.webapp.bean;
+
+public class User {
+	private String username;
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(String username) {
+
+		this.username = username;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+}
